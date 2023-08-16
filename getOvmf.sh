f8217555e7b9482dcdf98aeb6509117c122cb7c5
shotanue <shotanue@gmail.com>
2023-08-17 00:59:14 +0900

    setup build.zig

--- getOvmf.sh
+++ getOvmf.sh
@@ -1,14 +1,18 @@
 #! /bin/sh
 
+# cleanup
+rm -rf data.tar.xz control.tar.xz debian-binary source.deb usr
+
 curl -sSl http://ftp.jp.debian.org/debian/pool/main/e/edk2/qemu-efi-aarch64_2023.05-1_all.deb >source.deb
 
-ar -x ./source.deb
-tar -xf ./source.deb
-#rm ./*.fd
-# cp ./usr/share/AAVMF/AAVMF_CODE.fd ./AAVMF_CODE.fd
-# cp ./usr/share/AAVMF/AAVMF_VARS.fd ./AAVMF_VARS.fd
+ar -xv ./source.deb
 
-# cp ./usr/share/qemu-efi-aarch64/QEMU_EFI.fd ./QEMU_EFI.fd
+tar -xf ./data.tar.xz
+
+cp ./usr/share/AAVMF/AAVMF_CODE.fd ./AAVMF_CODE.fd
+cp ./usr/share/AAVMF/AAVMF_VARS.fd ./AAVMF_VARS.fd
+
+cp ./usr/share/qemu-efi-aarch64/QEMU_EFI.fd ./QEMU_EFI.fd
 
 # cleanup
-# rm ./*.tar.xz
+rm -rf data.tar.xz control.tar.xz debian-binary source.deb usr
--- main.zig
+++ main.zig
@@ -1,12 +1,28 @@
 const std = @import("std");
-const uefi = @import("std").os.uefi;
+const uefi = std.os.uefi;
 
-pub fn main() !void {
-    const system_table = uefi.system_table;
-    const stdout = system_table.BootServices.con_out;
+var con_out: *uefi.protocols.SimpleTextOutputProtocol = undefined;
 
-    try stdout.clear();
-    try stdout.outputString("Hello, World from UEFI!\n");
+fn printf(comptime format: []const u8, args: anytype) void {
+    var buf: [1024]u8 = undefined;
+    const text = std.fmt.bufPrint(&buf, format, args) catch unreachable;
+    for (text) |c| {
+        con_out.outputString(&[_:0]u16{ c, 0 }).err() catch unreachable;
+    }
+}
 
-    return null;
+pub fn main() uefi.Status {
+    con_out = uefi.system_table.con_out orelse return .Unsupported;
+    // const system_table = uefi.system_table;
+    // const stdout = system_table.con_out.?;
+
+    var status = con_out.clearScreen();
+    if (status != .Success) {
+        printf("failed to clear screen: {d}\r\n", .{status});
+        return status;
+    }
+    // if (uefi.Status.Success != result) {}
+    printf("Hello, World from UEFI!\n", .{});
+
+    return .Success;
 }
